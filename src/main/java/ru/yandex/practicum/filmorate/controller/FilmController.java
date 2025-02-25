00539f7b2211bd5e78e3a87aee79ffad32be8f3a
DeanMave <dean.tarantul@gmail.com>
2025-02-25 16:25:06 +0300

    Codestyle x1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -44,8 +44,8 @@ public class FilmController {
         film.setId(getNextId());
         films.put(film.getId(), film);
         log.info("Новый фильм успешно добавлен: ID = {}, Название фильма = {}, Описание фильма = {}, " +
-                        "Дата релиза = {}, Длительность = {}"
-                , film.getId(), film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration());
+                        "Дата релиза = {}, Длительность = {}",
+                film.getId(), film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration());
         return film;
     }
 
@@ -79,8 +79,8 @@ public class FilmController {
             oldFilm.setReleaseDate(newFilm.getReleaseDate());
             oldFilm.setDuration(newFilm.getDuration());
             log.info("Данные фильма успешно обновлены: ID = {}, Название фильма = {}, Описание фильма = {}, " +
-                            "Дата релиза = {}, Длительность = {}"
-                    , oldFilm.getId(), oldFilm.getName(), oldFilm.getDescription(), oldFilm.getReleaseDate(), oldFilm.getDuration());
+                            "Дата релиза = {}, Длительность = {}",
+                    oldFilm.getId(), oldFilm.getName(), oldFilm.getDescription(), oldFilm.getReleaseDate(), oldFilm.getDuration());
             return oldFilm;
         }
         log.warn("Ошибка при обновлении фильма: фильм с ID {} не найден", newFilm.getId());
