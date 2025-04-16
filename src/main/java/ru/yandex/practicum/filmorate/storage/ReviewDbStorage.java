ef0edcc8872b9a442be622229ad5094d62f38649
DeanMave <dean.tarantul@gmail.com>
2025-04-16 18:34:16 +0300

    Финальные исправления

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewDbStorage.java
@@ -48,23 +48,27 @@ public class ReviewDbStorage implements ReviewStorage {
 
     @Override
     public List<Review> getFilmReviews(Integer filmId, Integer count) {
-        String sql = "SELECT r.review_id, r.film_id, r.user_id, r.content, r.is_positive, " +
-                "(SELECT COUNT(*) FROM review_likes WHERE review_id = r.review_id) - " +
-                "(SELECT COUNT(*) FROM review_dislikes WHERE review_id = r.review_id) AS useful " +
-                "FROM reviews r " +
-                "WHERE r.film_id = ? LIMIT ?";
+        String sql = """
+                SELECT r.review_id, r.film_id, r.user_id, r.content, r.is_positive,
+                       (SELECT COUNT(*) FROM review_likes WHERE review_id = r.review_id) -
+                       (SELECT COUNT(*) FROM review_dislikes WHERE review_id = r.review_id) AS useful
+                FROM reviews r
+                WHERE r.film_id = ?
+                ORDER BY useful DESC
+                LIMIT ?
+                """;
         return jdbcTemplate.query(sql, reviewRowMapper, filmId, count);
     }
 
     @Override
     public List<Review> getUserReviews(Integer userId) {
         String sql = "SELECT r.review_id, r.film_id, r.user_id, r.content, r.is_positive, "
-                + "(COUNT(DISTINCT rl.user_id) - COUNT(DISTINCT rd.user_id)) AS useful "
-                + "FROM reviews r "
-                + "LEFT JOIN review_likes rl ON r.review_id = rl.review_id "
-                + "LEFT JOIN review_dislikes rd ON r.review_id = rd.review_id "
-                + "WHERE r.user_id = ? " // Исправлено film_id на user_id
-                + "GROUP BY r.review_id, r.film_id, r.user_id, r.content, r.is_positive";
+                     + "(COUNT(DISTINCT rl.user_id) - COUNT(DISTINCT rd.user_id)) AS useful "
+                     + "FROM reviews r "
+                     + "LEFT JOIN review_likes rl ON r.review_id = rl.review_id "
+                     + "LEFT JOIN review_dislikes rd ON r.review_id = rd.review_id "
+                     + "WHERE r.user_id = ? "
+                     + "GROUP BY r.review_id, r.film_id, r.user_id, r.content, r.is_positive";
 
         return jdbcTemplate.query(sql, reviewRowMapper, userId);
     }
@@ -105,10 +109,10 @@ public class ReviewDbStorage implements ReviewStorage {
     @Override
     public Optional<Review> getReviewById(Integer id) {
         String sql = "SELECT r.review_id, r.film_id, r.user_id, r.content, r.is_positive, "
-                + "(SELECT COUNT(*) FROM review_likes WHERE review_id = r.review_id) - "
-                + "(SELECT COUNT(*) FROM review_dislikes WHERE review_id = r.review_id) AS useful "
-                + "FROM reviews r "
-                + "WHERE r.review_id = ?";
+                     + "(SELECT COUNT(*) FROM review_likes WHERE review_id = r.review_id) - "
+                     + "(SELECT COUNT(*) FROM review_dislikes WHERE review_id = r.review_id) AS useful "
+                     + "FROM reviews r "
+                     + "WHERE r.review_id = ?";
 
         try {
             return Optional.ofNullable(jdbcTemplate.queryForObject(sql, reviewRowMapper, id));
@@ -120,11 +124,11 @@ public class ReviewDbStorage implements ReviewStorage {
     @Override
     public List<Review> getAllReviews(int count) {
         String sql = "SELECT r.review_id, r.film_id, r.user_id, r.content, r.is_positive, "
-                + "(SELECT COUNT(*) FROM review_likes WHERE review_id = r.review_id)  - "
-                + "(SELECT COUNT(*) FROM review_dislikes WHERE review_id = r.review_id) AS useful "
-                + "FROM reviews r "
-                + "ORDER BY useful DESC "
-                + "LIMIT ?";
+                     + "(SELECT COUNT(*) FROM review_likes WHERE review_id = r.review_id)  - "
+                     + "(SELECT COUNT(*) FROM review_dislikes WHERE review_id = r.review_id) AS useful "
+                     + "FROM reviews r "
+                     + "ORDER BY useful DESC "
+                     + "LIMIT ?";
 
         return jdbcTemplate.query(sql, reviewRowMapper, count);
     }
